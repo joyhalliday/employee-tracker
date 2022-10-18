ce555e5e524f05606ca1d5784add0b5fae1e1171
Joy Halliday <tjoyhalliday@gmail.com>
2022-10-18 14:09:18 -0700

    added base files and installs

---
+++ index.js
@@ -0,0 +1,19 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+{
+    host: 'localhost',
+    user: 'root',
+    password: 'YourPassword',
+    database: 'employee_db'
+},
+console.log(`Connected to the employee_db database.`)
+);
