85cf113add0c82fbf6d79e690336b7ef7888c78e
Joy Halliday <tjoyhalliday@gmail.com>
2022-10-18 21:27:10 -0700

    added input information to seed file

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    role_id INT,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES roles(id),
+    ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (dept_name)
+VALUES ("Finance"),
+    ("Software"),
+    ("Marketing"),
+    ("Human Resources"),
+    ("Graphic Design");
+
+INSERT INTO roles (title, salary, department_id) 
+VALUES ("Finance Director", 96000, 1),
+        ("Finance Associate", 70000, 1),
+        ("Software Developer", 120000, 2),
+        ("Junior Software Developer", 80000, 2),
+        ("Marketing Director", 75000, 3),
+        ("Marketing Assistant", 50000, 3),
+        ("HR Director", 75000, 4),
+        ("HR Assistant", 50000, 4),
+        ("Graphic Designer", 70000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Homer", "Simpson", 1),
+        ("Ned", "Flanders", 2),
+        ("Clancy", "Wiggam", 3),
+        ("Selma", "Bouvier", 4),
+        ("Seymour", "Skinner", 5),
+        ("Lenny", "Leonard", 6),
+        ("Carl", "Carlson", 7),
+        ("Barney", "Gumble", 8),
+        ("Helen", "Lovejoy", 9);
+
